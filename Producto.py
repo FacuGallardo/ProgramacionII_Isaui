3337b660864b95a705dd9a8e36df9557cb92a754
FacuGallardo <166397923+FacuGallardo@users.noreply.github.com>
2024-06-11 17:51:17 -0300

    Add files via upload

---
+++ Producto.py
@@ -0,0 +1,9 @@
+#Definir productito, la clase padrino
+class Producto:
+    def __init__(self,nombre,precio,cantidad):
+        self.nombre = nombre
+        self.precio = precio
+        self.cantidad = cantidad
+    
+def mostrar_informacion(self):
+    return f"Nombre: {self.nombre}, Precio: {self.precio}, Cantidad: {self.cantidad}"
